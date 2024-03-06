7ed58c1f328ea2b9c7a1bdba5eb07fda59cfc98b
song33ztpgg <sasasa7737@gmail.com>
2024-03-06 16:35:45 +0900

    remove : 동규님꺼 받기 전에 충돌 파일 삭제

--- src/main/kotlin/com/jspl/tickettaka/repository/FacilityDetailRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.jspl.tickettaka.repository
-
-import com.jspl.tickettaka.model.FacilityDetail
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface FacilityDetailRepository:JpaRepository<FacilityDetail, Long> {
-}
--- src/main/kotlin/com/jspl/tickettaka/repository/PerformanceRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.jspl.tickettaka.repository
-
-import com.jspl.tickettaka.model.Performance
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PerformanceRepository:JpaRepository<Performance, Long> {
-}
