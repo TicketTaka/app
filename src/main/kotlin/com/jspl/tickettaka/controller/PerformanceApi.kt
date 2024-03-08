a5a1b4c4a498ca6858e4339baa4a15b405f8422a
song33ztpgg <sasasa7737@gmail.com>
2024-03-08 16:25:52 +0900

    fix : 삭제한 파일복구

---
+++ src/main/kotlin/com/jspl/tickettaka/controller/PerformanceApi.kt
@@ -0,0 +1,19 @@
+package com.jspl.tickettaka.controller
+
+import com.jspl.tickettaka.service.PerformanceService
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequiredArgsConstructor
+class PerformanceApi(
+    private val performanceService: PerformanceService
+) {
+    @GetMapping("/update-performances")
+    fun update() {
+        performanceService.updatePerformance()
+        println("complete")
+    }
+
+}
