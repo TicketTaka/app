fb65d4cd1de09f8ca9bf2badbde0f7021e839a83
parkseongryel <tjdfuf3413@gmail.com>
2024-03-11 17:14:09 +0900

    feat: 공연 등록시 채널 추가 및 알림 전송

--- src/main/kotlin/com/jspl/tickettaka/controller/PerformanceApi.kt
+++ src/main/kotlin/com/jspl/tickettaka/controller/PerformanceApi.kt
@@ -3,6 +3,7 @@ package com.jspl.tickettaka.controller
 import com.jspl.tickettaka.service.PerformanceService
 import lombok.RequiredArgsConstructor
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,8 +12,8 @@ class PerformanceApi(
     private val performanceService: PerformanceService
 ) {
     @GetMapping("/update-performance")
-    fun update() {
-        performanceService.updatePerformance()
+    fun update(@RequestParam genre: String, @RequestParam title: String,) {
+        performanceService.updatePerformance(genre, title)
         println("complete")
     }
 }
--- src/main/kotlin/com/jspl/tickettaka/service/PerformanceService.kt
+++ src/main/kotlin/com/jspl/tickettaka/service/PerformanceService.kt
@@ -1,15 +1,22 @@
 package com.jspl.tickettaka.service
 
 import com.jspl.tickettaka.data.PerformanceDataCrawling
-import com.jspl.tickettaka.model.Performance
-import com.jspl.tickettaka.repository.PerformanceRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
 
 @Service
-class PerformanceService(
+class PerformanceService @Autowired constructor(
     private val performanceDataCrawling: PerformanceDataCrawling,
+    private val redisTemplate: RedisTemplate<String, String>
 ) {
-    fun updatePerformance() {
+    fun updatePerformance(genre: String, title: String) {
         performanceDataCrawling.execute()
+
+        val channel = "genre:$genre"
+        redisTemplate.opsForList().rightPushIfPresent("channels", channel)
+
+        val message = "새로운 공연이 등록되었습니다: $title"
+        redisTemplate.convertAndSend(channel, message)
     }
 }
