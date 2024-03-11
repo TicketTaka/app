cdab7db2c39ade91c0a5727251ef365859d0ded9
parkseongryel <tjdfuf3413@gmail.com>
2024-03-11 14:31:52 +0900

    remove: Notification

--- src/main/kotlin/com/jspl/tickettaka/repository/NotificationRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.jspl.tickettaka.repository
-
-import com.jspl.tickettaka.model.Notification
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface NotificationRepository : JpaRepository<Notification, Long> {
-}
